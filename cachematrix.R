d78394b93de418bce79af7879d35e8dc4df340a5
Tikigami <hritik03arora@gmail.com>
2022-02-05 13:08:23 +0530

    Updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+library(matrixcalc) ##The function needs the package matrixcalc to work
 
-## Write a short comment describing this function
+##These two functions calculate the inverse of a matrix and caches it, 
+##make sure to store your matrix as an object before using the functions
 
-makeCacheMatrix <- function(x = matrix()) {
-
+##makeCachematrix takes a matrix (only square), calculates its inverse and caches it
+makeCacheMatrix <- function(x = matrix()){
+  
+  m <- NULL
+  set <- function(y){
+      x <<- y
+      m <<- NULL
+  }
+  get <- function() x
+  ##if the matrix is a square matrix, it calculates its inverse
+  setinver <- if (is.square.matrix(x) == TRUE){
+          function(solve) m <<- solve
+  } ## if the matrix is not a square matrix an error occurs
+    else{
+          message("the given matrix is not a square matrix")
+          stop()
+  }
+  getinver <- function() m
+  list(x, set = set, get = get, setinver = setinver, getinver = getinver)
+  
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function returns the inverse stored in the cache or calculates the inverse if it has not been done yet
+cacheSolve <- function(x, ...){
+  
+  m <- x$getinver()
+  if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+  }
+  data <-x$get()
+  m <- if (is.square.matrix(data) == TRUE){
+        solve(data, ...)
+  } else{
+        message("the given matrix is not a square matrix")
+  }
+  x$setinver(m)
+  m
 }
